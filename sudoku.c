e5cc5c16b6f0922ad9e5fa8af77450bed40e540a
Your Name <you@example.com>
2024-05-16 17:41:34 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -43,8 +43,48 @@ void print_node(Node* n){
     printf("\n");
 }
 
-int is_valid(Node* n){
+/*
+   2.Cree la función int is_valid(Node * n), para validar si un estado/nodo es válido (cumple con las restricciones del problema). Debe validar que:
 
+   No se repitan números en las filas
+   No se repitan números en las columnas
+   No se repitan números en las submatrices de 3x3
+   Si el estado es válido la función retorna 1, si no lo es retorna 0.
+
+   Para marcar los números que vayan apareciendo en una fila/columna/submatriz puede usar un arreglo de enteros de largo 10 inicializado con 0s. Cada vez que aparezca un número i, verifique que la casilla i del arreglo sea igual a 0, luego márquela con un '1'. Si la casilla es '1' quiere decir que el número ya estaba marcado por lo que la fla/columna/submatriz no es válida.
+*/
+
+int is_valid(Node* n){
+   int filas_check[9][9] = {0};
+   int columnas_check[9][9] = {0};
+   int submatriz_check[9][9] = {0};
+   int i,j;
+   for(i=0;i<9;i++){
+      for(j=0;j<9;j++){
+         int num = n->sudo[i][j];
+         if(num != 0){
+            //Si es distinto de 0, verificar que no se repita en la fila
+            if(filas_check[i][num-1] == 1){
+               return 0;
+            }
+            filas_check[i][num-1] = 1;
+            //Verificar que no se repita en la columna
+            if(columnas_check[j][num-1] == 1){
+               return 0;
+            }
+            columnas_check[j][num-1] = 1;
+            //Verificar que no se repita en la submatriz
+            int submatriz_fila = i / 3;
+            int submatriz_columna = j / 3;
+            if(submatriz_check[submatriz_fila][submatriz_columna][num-1] == 1){
+               return 0;
+            }
+            submatriz_check[submatriz_fila][submatriz_columna][num-1];
+            
+            
+         }
+      }
+   }
     return 1;
 }
 
@@ -64,6 +104,8 @@ Node* DFS(Node* initial, int* cont){
 }
 
 
+
+/*
 int main( int argc, char *argv[] ){
 
   Node* initial= read_file("s12a.txt");;
@@ -74,4 +116,4 @@ int main( int argc, char *argv[] ){
   print_node(final);
 
   return 0;
-}
+}*/
